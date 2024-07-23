6ab1b4729c7934c2f45650ed81f01401ca127d16
flipmeup <pavanmeena9308@gmail.com>
2024-07-23 13:56:11 +0530

    Update Payment.jsx

--- Frontend/src/components/Payment/Payment.jsx
+++ Frontend/src/components/Payment/Payment.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+
 import { useLocation } from 'react-router-dom';
 import PaymentNav from '../PaymentNav/PaymentNav';
 import { Modal, Button } from 'react-bootstrap';
